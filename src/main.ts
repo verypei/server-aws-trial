26a5ceb02d8cbb7ff2ae9a494b27399651852798
verypei <verrymariyanto@gmail.com>
2025-02-02 05:13:10 +0700

    fixing deploy.yml

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('aws');
-  await app.listen(3000, () => {
-    console.log("----------server running  on port 3000----------");
-  });
+  // await app.listen(3000, () => {
+  //   console.log("----------server running  on port 3000----------");
+  // });
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
