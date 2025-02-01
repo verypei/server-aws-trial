15f74a3000fc3fdec7ae3b14daa449e802f4c4a1
verypei <verrymariyanto@gmail.com>
2025-02-02 04:04:54 +0700

    firdt init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('aws');
+  await app.listen(3000, () => {
+    console.log("----------server running  on port 3000----------");
+  });
+}
+bootstrap();
