e1a4e42cb94777c9d0650716e401ac542f3e128a
verypei <verrymariyanto@gmail.com>
2025-02-03 00:04:12 +0700

    updatin g deploy.yml

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('aws');
-  // await app.listen(3000, () => {
-  //   console.log("----------server running  on port 3000----------");
-  // });
   await app.listen(3000, '0.0.0.0', () => {
     console.log("----------server running  on port 3000----------");
   });
