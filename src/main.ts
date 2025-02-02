8e549607457efe379c7fbf179e2fe6572581df61
verypei <verrymariyanto@gmail.com>
2025-02-02 09:21:03 +0700

    trial docker update

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   // await app.listen(3000, () => {
   //   console.log("----------server running  on port 3000----------");
   // });
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(3000, '0.0.0.0', () => {
+    console.log("----------server running  on port 3000----------");
+  });
 }
 bootstrap();
