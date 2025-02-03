ab1953d4726fbdffcd47f777563cd395cfbfe1d7
verypei <verrymariyanto@gmail.com>
2025-02-04 06:25:04 +0700

    trial https directly

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/api.segendigitalindonesia.id/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.segendigitalindonesia.id/fullchain.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+  // const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('aws');
-  await app.listen(3000, '0.0.0.0', () => {
-    console.log("----------server running  on port 3000- after docker build---------");
+  app.use(helmet());
+  await app.listen(433, '0.0.0.0', () => {
+    console.log("---------- server running  on port 433- TRIAL HTTPS --------");
   });
 }
 bootstrap();
