3c9f28d9a1d9cf0c10db9ed5a01b1844c1ad0859
verypei <verrymariyanto@gmail.com>
2025-02-04 06:31:55 +0700

    trial https directly

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import * as fs from 'fs';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/api.segendigitalindonesia.id/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/api.segendigitalindonesia.id/fullchain.pem'),
-  };
-  const app = await NestFactory.create(AppModule, { httpsOptions });
-  // const app = await NestFactory.create(AppModule);
+  // const httpsOptions = {
+  //   key: fs.readFileSync('/etc/letsencrypt/live/api.segendigitalindonesia.id/privkey.pem'),
+  //   cert: fs.readFileSync('/etc/letsencrypt/live/api.segendigitalindonesia.id/fullchain.pem'),
+  // };
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('aws');
   app.use(helmet());
   await app.listen(433, '0.0.0.0', () => {
