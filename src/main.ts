f3012692c5d3cc63c4026ad0989c4a190947a712
verypei <verrymariyanto@gmail.com>
2025-02-04 06:58:40 +0700

    trial https directly

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('aws');
   app.use(helmet());
-  await app.listen(433, '0.0.0.0', () => {
+  await app.listen(3000, '0.0.0.0', () => {
     console.log("---------- server running  on port 433- TRIAL HTTPS --------");
   });
 }
